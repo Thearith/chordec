6c0e80bec4fffdaa83f018d77fddae074ed42379
thearith <sothearith.sreang@gmail.com>
2015-03-26 00:43:13 +0800

    Add Main Activity and some drawables

---
+++ Chordec/app/src/main/java/com/example/chordec/chordec/CSurfaceView/SoundCSurfaceView.java
@@ -0,0 +1,7 @@
+package com.example.chordec.chordec.CSurfaceView;
+
+/**
+ * Created by thearith on 25/3/15.
+ */
+public class SoundCSurfaceView {
+}
--- Chordec/app/src/main/java/com/example/chordec/chordec/MainActivity.java
+++ Chordec/app/src/main/java/com/example/chordec/chordec/MainActivity.java
@@ -2,16 +2,65 @@ package com.example.chordec.chordec;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.RelativeLayout;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity
+    implements View.OnClickListener{
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    //    widgets in activity_main.xml
+    private ImageButton recordButton;
+    private ImageButton pauseButton;
+    private ImageButton stopButton;
+
+    //    layouts in activity_main.xml
+    private RelativeLayout recordLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // initialize widgets
+        recordButton = (ImageButton) findViewById(R.id.recordButton);
+        recordButton.setOnClickListener(this);
+
+        pauseButton = (ImageButton) findViewById(R.id.pauseButton);
+        pauseButton.setOnClickListener(this);
+
+        stopButton = (ImageButton) findViewById(R.id.stopButton);
+        stopButton.setOnClickListener(this);
+
+        // initialize layouts
+        recordLayout = (RelativeLayout) findViewById(R.id.recordLayout);
+
+    }
+
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.recordButton:
+
+                break;
+
+            case R.id.pauseButton:
+
+                break;
+
+            case R.id.stopButton:
+
+                break;
+
+            default:
+                Log.e(TAG, "Widget is not recognized");
+                break;
+        }
     }
 
 
@@ -30,10 +79,15 @@ public class MainActivity extends ActionBarActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_database) {
+            goToDatabase();
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void goToDatabase() {
+
     }
 }
